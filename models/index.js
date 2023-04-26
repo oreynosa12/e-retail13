54261c0139dcf4968f75ce880686cbafe1ebaece
OMR K9 <121472588+oreynosa12@users.noreply.github.com>
2023-04-25 23:36:30 -0400

    modified products and tags code

--- models/index.js
+++ models/index.js
@@ -6,22 +6,25 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey:'category_id'
+  foreignKey:'category_id',
+  onDelete: 'CASCADE'
+
 })
 // Categories have many Products
-Category.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey:'product_id'
+Category.hasMany(Product, {
+  foreignKey:'category_id'
 })
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
+  as: 'product_tags',
   foreignKey:'product_id'
 })
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey:'product_id'
+  as: 'product_tags',
+  foreignKey:'tag_id'
 })
 module.exports = {
   Product,
